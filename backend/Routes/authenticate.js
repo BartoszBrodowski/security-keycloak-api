b0baed57379d76a0575b6e808269279d6417921c
BartoszBrodowski <bartolini10058@gmail.com>
2023-06-20 22:06:52 +0200

    Current version working properly, fixed bug with jwt verify

--- backend/Routes/authenticate.js
+++ backend/Routes/authenticate.js
@@ -1,19 +1,23 @@
 import jwtmod from "jsonwebtoken";
 
 const func = async (req, res, next) => {
-  const bearerHeader = req.headers["authorization"];
-  const token = bearerHeader && bearerHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
+  try {
+    const bearerHeader = req.headers["authorization"];
+    const token = bearerHeader && bearerHeader.split(" ")[1];
+    if (token == null) return res.sendStatus(401);
 
-  const public_key = `-----BEGIN PUBLIC KEY-----\n${process.env.PUBLICKEY}\n-----END PUBLIC KEY-----`;
+    const public_key = `-----BEGIN PUBLIC KEY-----\n${process.env.PUBLICKEY}\n-----END PUBLIC KEY-----`;
+    const decodedToken = jwtmod.verify(token, public_key, {
+      algorithms: ["RS256"],
+    });
 
-  const decodedToken = jwtmod.verify(token, public_key, {
-    algorithms: ["RS256"],
-  });
-
-  const { email } = decodedToken;
-  req.user = email;
-  next();
+    const { email } = decodedToken;
+    req.user = email;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(403).send("Invalid Token");
+  }
 };
 
 export default func;
